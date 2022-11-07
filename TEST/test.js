f9a13b1daa882a37b6618c59e5aed78626c2ec20
Chukwuka Paul Ezeh <chuksintellectual@gmail.com>
2022-11-07 17:47:01 +0100

    test added

---
+++ TEST/test.js
@@ -0,0 +1,28 @@
+const cat = { name : 'Bertie', breed : 'Cymric', color : 'white', greeting: function() { console.log('Meotttuuyuyuw!'); } }
+
+//Question 1: In this task, your tasks are to 1a)Store the value of the name property inside the catName variable, using bracket notation. 
+//1b)Run the greeting() (it will log the greeting to the browser's console). 1c)Update the color property value to black.
+
+
+const catName ={
+    name: 'Bertie',
+  
+};
+const greeting = function(greet){
+console.log(greet)
+
+}
+
+greeting();
+
+// Object.key(color).filter(color =>{
+//     color[key.color] = 'black'
+// })
+console.log(cat)
+//catName.greeting();
+
+ cat.color = 'black'
+ console.log(cat);
+
+//document.tags.H1.color = "blue"
+//console.log(color.value)
